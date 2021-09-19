e5c861bcb0ef6ae111b34b9fd2d428adeb720464
HeatherVroman <90171192+HeatherVroman@users.noreply.github.com>
2021-09-18 22:45:59 -0500

    Add files via upload

---
+++ 84082032assigment1.js
@@ -0,0 +1,51 @@
+
+        //the variable myNumber should be defined
+        var myNumber;
+        
+        //the variable myNumber should be type number
+        //the variable myNumber should be equal to 5
+        var myNumber = 5;
+        
+        //the variable of myOtherNumber should be defined
+        //the variable of myOtherNumber should be of type number
+        //the variable of myOtherNumber should be equal to 5
+        var myOtherNumber=5;
+        
+        //the variable myTotal should be defined
+        //the variable myTotal should be type number
+        //the variable myTotal should be equal to the product of myNumber and myOtherNumber
+        var myTotal=myNumber*myOtherNumber;
+        
+        //the variable myName should be defined
+        //the variable myName should be type string
+        //the variable myCombo should be defined
+        //the variable myCombo should be type string
+        //the variable myCombo should be equal to the sum of myNumber and myOtherNumber
+       let myName='Heather';
+       let myCombo=myNumber+myOtherNumber;
+       
+       //the variable myOtherCombo should be defined
+       //the variable myOtherCombo should be type number
+       //the variable myOtherCombo should be equal to the sum of myNumber and myOtherNumber
+       var myOtherCombo=myNumber+myOtherNumber;
+       
+       //the variable myRemainder should be defined
+       //the variable myRemainder should be type number
+       //the variable myRemainder should be equal to the remainder of myNumber divided by 5
+       var myRemainder=myNumber%5;
+       
+       //the variable i should be defined
+       //the variable i should be type number
+       //the variable i should be equal to 2
+       var i=2;
+       
+       //the variable int should be defined
+       //the variable int should be type number
+       //the variable int should be type 4
+       var int=4;
+       
+       //the variable myBoolean should be defined
+       //the variable myBoolean should be type boolean
+       //the variable myBoolean should be false
+       let myBoolean=false;
+    
